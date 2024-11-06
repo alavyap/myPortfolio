a148056749cae81fd3efb0282e59e85d69bb6460
Aalavya <alavya.pandey@gmail.com>
2024-11-06 07:19:00 +0530

    minor update

--- components/intro.tsx
+++ components/intro.tsx
@@ -67,9 +67,9 @@ const Intro = () => {
       >
         <span className="font-bold">Hello, I'm Alavya.</span> I'm a{" "}
         <span className="font-bold">front-end developer</span> with{" "}
-        <span className="font-bold">3+ years</span> of experience. I enjoy
+        <span className="font-bold">2 years</span> of experience. I enjoy
         building <span className="italic">sites & apps</span>. My focus is{" "}
-        <span className="underline">MERN (React)</span>.
+        <span className="underline">Frontend Development (React)</span>.
       </motion.h1>
 
       <motion.div
